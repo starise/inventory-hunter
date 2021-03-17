9db1e6795868de1cb49f70109d606f82d96ec8fe
Andrea Brandi <git@andreabrandi.com>
2021-03-17 18:10:47 +0100

    Add languages to common scraper Adds: italian, german, french, spanish

--- src/scraper/common.py
+++ src/scraper/common.py
@@ -1,4 +1,5 @@
 import datetime
+import html
 import locale
 import logging
 import re
@@ -39,6 +40,7 @@ class ScrapeResult(ABC):
             return
 
         price_str = tag if isinstance(tag, str) else tag.text.strip()
+        price_str = html.unescape(price_str).strip()
         if not price_str:
             return
 
@@ -68,7 +70,12 @@ class ScrapeResult(ABC):
 class GenericScrapeResult(ScrapeResult):
     def parse(self):
         # not perfect but usually good enough
-        if self.has_phrase('add to cart') or self.has_phrase('add to basket'):
+        if (self.has_phrase('add to cart')
+                or self.has_phrase('add to basket')
+                or self.has_phrase('aggiungi al carrello')
+                or self.has_phrase('in den warenkorb')
+                or self.has_phrase('ajouter au panier')
+                or self.has_phrase('añadir a la cesta')):
             self.alert_subject = 'In Stock'
             self.alert_content = self.url
 
